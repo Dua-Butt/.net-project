7408b8aab0ebe8aba67b20dd3668b638a71ca39c
Dua-Butt <bdua9833@gmail.com>
2025-09-16 17:38:20 +0500

    Added syntax error to fail build

--- Program.cs
+++ Program.cs
@@ -1,1 +1,3 @@
-int x = ;
+using Microsoft.AspNetCore.Hosting;  // Ye line already hogi, but add this invalid one below it
+
+int x = "hello";  // ERROR: Type mismatch - int mein string assign nahi ho sakti. Ye build fail karega!
