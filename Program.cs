b3f03a1dc8dec278cbe440d54691fb97083d75e6
Dua-Butt <bdua9833@gmail.com>
2025-09-17 11:13:23 +0500

    Fail

--- Program.cs
+++ Program.cs
@@ -1,3 +1,3 @@
  // Ye line already hogi, but add this invalid one below it
 
-int x = "hello";  // ERROR: Type mismatch - int mein string assign nahi ho sakti. Ye build fail karega!
+int x = ;  // ERROR: Type mismatch - int mein string assign nahi ho sakti. Ye build fail karega!
